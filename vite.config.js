6ae27a9955c5b1e77d42de7f1e3bbf8611327a09
freeway26tw <freeway26tw@gmail.com>
2023-07-18 11:17:30 +0800

    feat: scss doc auto import

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // 导入对应包
-import ElementPlus from 'unplugin-element-plus/vite'
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,7 +12,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver({ importStyle: "sass"})],
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
     }),
   ],
   resolve: {
@@ -27,6 +26,7 @@ export default defineConfig({
         // 自动导入定制化样式文件进行样式覆盖
         additionalData: `
           @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
         `,
       },
     },
