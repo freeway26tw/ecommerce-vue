31ab42403385705c89c8a6c116628e8d23fa0ada
freeway26tw <freeway26tw@gmail.com>
2023-07-19 19:31:56 +0800

    feat: detail-route setting

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -18,7 +18,7 @@ onMounted(() => getNewList())
   <HomePanel title="新鮮好物" subTitle="新鮮出爐 品質美賣">
     <ul class="goods-list">
       <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
           <img :src="item.picture" alt="" />
           <p class="name">{{ item.name }}</p>
           <p class="price">&yen;{{ item.price }}</p>
