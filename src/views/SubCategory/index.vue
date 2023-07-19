4b75d357eea3e5d03d8bd718a737d5c489340cbb
freeway26tw <freeway26tw@gmail.com>
2023-07-19 18:31:34 +0800

    feat: second-category infinite load

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -26,6 +26,25 @@ const getGoodList = ( async() => {
 })
 onMounted(() => getGoodList())
 
+// tab切換callback
+const tabChange = () => {
+  reqData.value.page = 1
+  getGoodList()
+}
+
+// 加載更多
+const disabled = ref(false)
+const load = async () => {
+  // 獲取下一頁數據
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 加載完畢 停止監聽
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
+
 </script>
 
 <template>
@@ -40,12 +59,12 @@ onMounted(() => getGoodList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="good in goodList" :good="good" :key="good.id" />
       </div>
