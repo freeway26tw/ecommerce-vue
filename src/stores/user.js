c8f77afed5982b6c3e599a037c718ef66c5fe89f
freeway26tw <freeway26tw@gmail.com>
2023-07-20 23:07:04 +0800

    feat: login-pinia data persistency

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,17 +4,23 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/user'
 
-export const useUserStore = defineStore('user', () => {
-  // 1. 定義管理用戶數據的state
-  const userInfo = ref({})
-  // 2. 定義獲取API數據的action函數
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password})
-    userInfo.value = res.result
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    // 1. 定義管理用戶數據的state
+    const userInfo = ref({})
+    // 2. 定義獲取API數據的action函數
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password })
+      userInfo.value = res.result
+    }
+    // 3. 以object的方式把state與action return
+    return {
+      userInfo,
+      getUserInfo,
+    }
+  },
+  {
+    persist: true,
   }
-  // 3. 以object的方式把state與action return
-  return {
-    userInfo,
-    getUserInfo
-  }
-})
+)
