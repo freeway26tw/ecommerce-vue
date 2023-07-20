9ae6762785a25d5365ba47f3bc2569499eace10b
freeway26tw <freeway26tw@gmail.com>
2023-07-20 16:46:22 +0800

    feat: sku component

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 import { getDetail } from '@/apis/detail'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
@@ -12,6 +13,11 @@ const getGoods = async () => {
   goods.value = res.result
 }
 onMounted(() => getGoods())
+
+// sku規格被操作時
+const skuChange = (sku) => {
+  console.log(sku);
+}
 </script>
 
 <template>
@@ -87,7 +93,7 @@ onMounted(() => getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
