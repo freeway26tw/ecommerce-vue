e2d1a0784426e99982f73522ddeaa715b62d006b
freeway26tw <freeway26tw@gmail.com>
2023-07-23 13:07:15 +0800

    feat: order - pagination

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -13,6 +13,7 @@ const tabTypes = [
 ]
 // 订单列表
 const orderList = ref([])
+const total = ref(0)
 const params = ref({
   orderState: 0,
   page: 1,
@@ -21,6 +22,7 @@ const params = ref({
 const getOrderList = async () => {
   const res = await getUserOrder(params.value)
   orderList.value = res.result.items
+  total.value = res.result.counts
 }
 
 onMounted(() => getOrderList())
@@ -28,6 +30,11 @@ onMounted(() => getOrderList())
 // tab切換
 const tabChange = (type) => {
   params.value.orderState = type
+  getOrderList()
+}
+
+const pageChange = (page) => {
+  params.value.page = page
   getOrderList()
 }
 
@@ -112,7 +119,7 @@ const tabChange = (type) => {
           </div>
           <!-- 分页 -->
           <div class="pagination-container">
-            <el-pagination background layout="prev, pager, next" />
+            <el-pagination :total="total" @current-change="pageChange" :page-size="params.pageSize" background layout="prev, pager, next" />
           </div>
         </div>
       </div>
